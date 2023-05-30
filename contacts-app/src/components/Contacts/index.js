2bc73a9a0fd3db64c2f068c218a5e585fcc78d66
Metin Keleş <metinkeles677@gmail.com>
2023-05-30 14:51:10 +0300

    FormComponents

--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+
 import List from './List'
 import Form from './Form'
 
-function index() {
+function Index() {
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        console.log(contacts);
+    }, [contacts])
+
     return (
         <div>
             <List />
-            <Form />
+            <Form addContact={setContacts} contacts={contacts} />
         </div>
     )
 }
 
-export default index
+export default Index
