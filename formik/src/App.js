812d9b3a018b8b6db55131b303f09e4ffdbbc8e1
Metin Keleş <metinkeles677@gmail.com>
2023-06-03 22:15:08 +0300

    UseFormik

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,43 +1,99 @@
 import './App.css';
-import { Formik, Field, Form } from 'formik'
+import { useFormik } from 'formik'
 
 function App() {
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      gender: 'male',
+      hobies: []
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="App">
-      <h1>Sign Up</h1>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input id="firstName" name="firstName" placeholder="Jane" value={values.firstName} onChange={handleChange} />
 
-          <br /><br />
+        <br /><br />
 
-          <label html For="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
+        <label html For="lastName">Last Name</label>
+        <input id="lastName" name="lastName" placeholder="Doe" value={values.lastName} onChange={handleChange} />
 
-          <br /><br />
+        <br /><br />
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
+        {/* radio */}
+        <span>Male</span>
+        <input
+          type="radio"
+          name='gender'
+          value="male"
+          onChange={handleChange}
+          checked={values.gender === "male"}
+        />
+
+        <span>Female</span>
+        <input
+          type="radio"
+          name='gender'
+          value="female"
+          onChange={handleChange}
+          checked={values.gender === "female"}
+        />
+        {/* radio */}
+
+        <br /><br />
+
+        {/* checkbox */}
+        <div>
+          <input
+            type="checkbox"
+            name='hobies'
+            value="cinema"
+            onChange={handleChange}
           />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
+          Cinema
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name='hobies'
+            value="Photography"
+            onChange={handleChange}
+          />
+          Photography
+        </div>
+        {/* checkbox */}
+
+
+        <select name='country' value={values.country} onChange={handleChange}>
+          <option value="turkey">turkey</option>
+          <option value="england">england</option>
+          <option value="usa">usa</option>
+        </select>
+
+        <br /><br />
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div >
   );
 }
 
