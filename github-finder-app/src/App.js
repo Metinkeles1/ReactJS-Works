3dcadf77750954d41bea7739cf310c12423dcb15
Metin Keleş <metinkeles677@gmail.com>
2023-06-08 11:53:18 +0300

    createUser&GithubAPI

--- github-finder-app/src/App.js
+++ github-finder-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import User from './components/User';
 
 function App() {
   return (
-    <div className="bg-primary">
+    <div>
       <Navbar icon="bi bi-github" title="Github Finder" />
+      <div className='container mb-3'>
+        <User />
+      </div>
     </div>
   );
 }
--- github-finder-app/src/components/Navbar.js
+++ github-finder-app/src/components/Navbar.js
@@ -13,15 +13,18 @@
 // export default Navbar
 
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
     render() {
         return (
             <nav className='navbar navbar-dark bg-primary'>
-                <a href='/' className='navbar-brand'>
-                    <i class={this.props.icon}></i> {this.props.title}
-                </a>
-            </nav>
+                <div className='container'>
+                    <a href='/' className='navbar-brand'>
+                        <i className={this.props.icon}></i> {this.props.title}
+                    </a>
+                </div>
+            </nav >
         )
     }
 }
@@ -29,6 +32,11 @@ Navbar.defaultProps = {
     icon: "bi bi-github",
     title: "Github Finder"
 };
+
+Navbar.propTypes = {
+    icon: PropTypes.string,
+    title: PropTypes.string
+}
 
 
 export default Navbar
