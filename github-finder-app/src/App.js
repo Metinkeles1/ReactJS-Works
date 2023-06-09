98bfe9751a430a04fee50aea7d59d3eeaa651fc6
Metin Keleş <metinkeles677@gmail.com>
2023-06-09 09:35:22 +0300

    searchComponent

--- github-finder-app/src/App.js
+++ github-finder-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import UserList from './components/UserList';
 import { Component } from 'react'
+import Search from './components/Search';
 
 export class App extends Component {
   constructor(props) {
@@ -13,20 +14,20 @@ export class App extends Component {
     }
   }
 
-  componentDidMount() {
+  searchUsers = (keyword) => {
     this.setState({ loading: true });
     setTimeout(() => {
-      fetch("https://api.github.com/users")
+      fetch("https://api.github.com/search/users?q=" + keyword)
         .then(response => response.json())
-        .then(data => this.setState({ users: data, loading: false }))
-    }, 2000);
-
+        .then(data => this.setState({ users: data.items, loading: false }))
+    }, 1000);
   }
 
   render() {
     return (
       <div>
         <Navbar icon="bi bi-github" title="Github Finder" />
+        <Search searchUsers={this.searchUsers} />
         <div className='container mb-3'>
           <UserList users={this.state.users} loading={this.state.loading} />
         </div>
