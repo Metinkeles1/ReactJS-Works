f5e59e468cb0626f1abf11e050ed6636091d6338
Metin Keleş <metinkeles677@gmail.com>
2023-06-25 21:09:20 +0300

    importRoutes

--- yemek-tarifleri/yemek-tarifleri/src/App.js
+++ yemek-tarifleri/yemek-tarifleri/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Details from './pages/details/Details';
+
+const routes = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/create', element: <Create /> },
+  { path: '/details/:id', element: <Details /> },
+  { path: '/search', element: <Search /> },
+])
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Nefis Yemek Tarifleri</h1>
-    </div>
+    <RouterProvider router={routes} />
   );
 }
 
