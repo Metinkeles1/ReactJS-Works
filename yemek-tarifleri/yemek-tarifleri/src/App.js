6486bde6f13cd97645c02fef011e8a34aaaf9b77
Metin Keleş <metinkeles677@gmail.com>
2023-06-26 12:04:54 +0300

    mainLayout

--- yemek-tarifleri/yemek-tarifleri/src/App.js
+++ yemek-tarifleri/yemek-tarifleri/src/App.js
@@ -6,12 +6,20 @@ import Home from './pages/home/Home';
 import Create from './pages/create/Create';
 import Search from './pages/search/Search';
 import Details from './pages/details/Details';
+import MainLayout from './layouts/MainLayout';
+import { Children } from 'react';
 
 const routes = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/create', element: <Create /> },
-  { path: '/details/:id', element: <Details /> },
-  { path: '/search', element: <Search /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/create', element: <Create /> },
+      { path: '/details/:id', element: <Details /> },
+      { path: '/search', element: <Search /> },
+    ]
+  }
 ])
 
 
