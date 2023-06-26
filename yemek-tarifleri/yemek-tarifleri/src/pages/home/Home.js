83cf6a478968a78f2ee9ef2bf9a14a138b3acda7
Metin Keleş <metinkeles677@gmail.com>
2023-06-26 13:40:39 +0300

    recipeList

--- yemek-tarifleri/yemek-tarifleri/src/pages/home/Home.js
+++ yemek-tarifleri/yemek-tarifleri/src/pages/home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../../components/ProductCard';
 import './Home.css';
 
 function Home() {
+    const [tarifler, setTarifler] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/tarifler")
+            .then(res => res.json())
+            .then(data => setTarifler(data))
+    }, []);
     return (
-        <div>Home</div>
+        <div className="row mt-3">
+            {
+                tarifler && tarifler.map(tarif => (
+                    <ProductCard key={tarif.id} tarif={tarif} />
+                ))
+            }
+        </div>
     )
 }
 
